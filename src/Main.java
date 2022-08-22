31b6fec0a4eb9ed3796f609dd318a74b5cc73544
sanychiquetti <sanymara77@hotmail.com>
2022-08-21 21:10:17 -0300

    atualizando e acrescentando pastas

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+			System.out.println("Hello, word!");
+			
+			int y = 32;
+			double x = 10.35784;
+			
+			System.out.println(y);
+			System.out.println(x);
+			System.out.printf("%.2f%n", x);
+			System.out.printf("%.4f%n", x);
+			System.out.println("Resultado = " + x + "metros");
+			System.out.printf("Resultado = %.2f metros%n", x);
+			
+			String nome = "Maria";
+			int idade = 31;
+			double renda = 4000.0;
+			
+			System.out.printf("%s tem %d anos e ganha R$ %.2f reais por mês%n", nome, idade, renda);
+			
+			
+			}
+
+}
